9f8d6bd33a4afbb69ff2b6eb15be9b432493d531
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-11-17 21:28:39 +0100

    deploy servlet using @WebServlet

--- src/main/java/org/suptech/tp01/MyServlet.java
+++ src/main/java/org/suptech/tp01/MyServlet.java
@@ -1,17 +1,19 @@
 package org.suptech.tp01;
 
-
-
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/login")
 public class MyServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("MyServlet1");
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("<h1>Connection Successful</h1>");
     }
 }
--- src/main/java/org/suptech/tp01/MyServlet2.java
+++ src/main/java/org/suptech/tp01/MyServlet2.java
@@ -2,11 +2,13 @@ package org.suptech.tp01;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/subscribe")
 public class MyServlet2 extends HttpServlet {
 
     @Override
